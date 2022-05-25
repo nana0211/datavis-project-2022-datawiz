f5021c82804e540ce752e731e79b3b9c10fef639
Jonas Konrad <jonas.konrad@epfl.ch>
2022-05-25 23:03:50 +0200

    move edition selection to edition tab

--- website/js/map.js
+++ website/js/map.js
@@ -29,7 +29,7 @@ sidebar
         id: "edition",
         tab: '📅',
         title: "Edition",
-        pane: '<p style="padding-top: 1em;">This tab allows for edition selection and contains information on the currently selected edition.</p>',
+        pane: '<p style="padding-top: 1em;">This tab allows for edition selection and contains information on the currently selected edition.</p><div class="container py-4"><select id="edition_select" class="form-select" aria-label="Default select example"></select></div>',
     })
 // add stages tab
 fetch("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/winner_tables/website/html/stage_tab.html")
