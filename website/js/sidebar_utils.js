524e25b287bc9b167c58567b70dbe52e40886294
ntian <nana.tian@epfl.ch>
2022-05-30 21:39:19 +0200

    os

--- website/js/sidebar_utils.js
+++ website/js/sidebar_utils.js
@@ -19,7 +19,7 @@ function init_edition_selection() {
         d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/stage_data.csv"),
         d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/tdf_winners.csv"),
         d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/flags.csv"),
-        d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/history.csv"),
+        d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/TDF_Riders_History.csv"),
     ]).then(function(initialize) {
         locations = initialize[0];
         stages = initialize[1]
